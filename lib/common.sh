5b13530430f1e8b044cd7e4eb14759e5a3e77a58
Brenton Morris <brentonamorris@gmail.com>
2015-05-22 14:40:00 -0400

    Check NODE_MODULES_CACHE and determine whether or not   to bust the node cache

---
+++ lib/common.sh
@@ -0,0 +1,28 @@
+clean_npm() {
+  status "Cleaning up node-gyp and npm artifacts"
+  rm -rf "$build_dir/.node-gyp"
+  rm -rf "$build_dir/.npm"
+}
+
+clean_cache() {
+  info "Cleaning previous cache"
+  rm -rf "$cache_dir/node_modules" # (for apps still on the older caching strategy)
+  rm -rf "$cache_dir/node"
+}
+
+create_cache() {
+  status "Caching node_modules directory for future builds"
+  cp -r $build_dir/node_modules $cache_dir/node
+}
+
+write_user_cache() {
+  local directories=($(cache_directories))
+  if [ "$directories" != -1 ]; then
+    info "Storing directories:"
+    for directory in "${directories[@]}"
+    do
+      info "- $directory"
+      cp -r $build_dir/$directory $cache_dir/node/
+    done
+  fi
+}
