9548f43df1fe59aa17a5d87db028d25cde950d29
Dan Langevin <dlangevin@vericred.com>
2015-06-16 10:36:49 -0400

    Opt-in cache usage

--- lib/common.sh
+++ lib/common.sh
@@ -6,28 +6,44 @@ clean_npm() {
 
 clean_node_cache() {
   status "Cleaning node cache"
-  rm -rf "$cache_dir/node_modules" # (for apps still on the older caching strategy)
+  # (for apps still on the older caching strategy)
+  rm -rf "$cache_dir/node_modules"
   rm -rf "$cache_dir/node"
-
-  rm -rf "$build_dir/node_modules"
-  rm -rf "$build_dir/node"
 
   npm cache clean
 }
 
-create_cache() {
+clean_bower_cache() {
+  status "Cleaning bower cache"
+  rm -rf "$cache_dir/bower_components"
+}
+
+create_bower_cache() {
+  status "Caching bower_components directory for future builds"
+  clean_bower_cache
+  cp -r $build_dir/bower_components $cache_dir/node
+}
+
+create_node_cache() {
   status "Caching node_modules directory for future builds"
+  clean_node_cache
   cp -r $build_dir/node_modules $cache_dir/node
 }
 
-write_user_cache() {
-  local directories=($(cache_directories))
-  if [ "$directories" != -1 ]; then
-    info "Storing directories:"
-    for directory in "${directories[@]}"
-    do
-      info "- $directory"
-      cp -r $build_dir/$directory $cache_dir/node/
-    done
+restore_node_cache() {
+  # Restore bower_components from cache if it exists
+  rm -rf $build_dir/node_modules
+  if test -d $cache_dir/node_modules; then
+    status "Restoring node_modules directory from cache"
+    cp -r $cache_dir/node $build_dir/node_modules
+  fi
+}
+
+restore_bower_cache() {
+  # Restore bower_components from cache if it exists
+  rm -rf $build_dir/bower_components
+  if test -d $cache_dir/bower_components; then
+    status "Restoring bower_components directory from cache"
+    cp -r $cache_dir/bower_components $build_dir/
   fi
 }
