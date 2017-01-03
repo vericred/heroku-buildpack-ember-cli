06ff64cb7d977721b680f2a2ced9883caaba2471
Dan Langevin <dlangevin@vericred.com>
2017-01-03 09:30:27 -0500

    Removing unnecessary touch call

--- config/htpasswd.rb
+++ config/htpasswd.rb
@@ -1,12 +1,10 @@
 exit 0 unless ENV["BASIC_AUTH_USER"] && ENV["BASIC_AUTH_PASSWORD"]
 
 require "webrick"
-require "fileutils"
 
 DIR = ARGV[0]
 
 file = "#{DIR}/htpasswd"
-FileUtils.touch(file)
 
 htpasswd = WEBrick::HTTPAuth::Htpasswd.new(file)
 htpasswd.set_passwd(nil, ENV["BASIC_AUTH_USER"], ENV["BASIC_AUTH_PASSWORD"])
