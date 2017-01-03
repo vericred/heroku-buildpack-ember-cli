efe740341c94338c0b6094aaa3e24e15aa9747cb
Dan Langevin <dan.langevin@gmail.com>
2017-01-02 21:59:07 -0500

    More htpasswd stuff

--- config/htpasswd.rb
+++ config/htpasswd.rb
@@ -1,8 +1,12 @@
 exit 0 unless ENV["BASIC_AUTH_USER"] && ENV["BASIC_AUTH_PASSWORD"]
 
 require "webrick"
+require "fileutils"
 
 file = "#{ENV["HOME"]}/config/htpasswd"
+FileUtils.mkdir_p("#{ENV["HOME"]}/config")
+FileUtils.touch(file)
+
 htpasswd = WEBrick::HTTPAuth::Htpasswd.new(file)
 htpasswd.set_passwd(nil, ENV["BASIC_AUTH_USER"], ENV["BASIC_AUTH_PASSWORD"])
 htpasswd.flush
