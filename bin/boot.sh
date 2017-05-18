f65ae36d8718bd251bb7db15a223e052548626e7
Dan Langevin <dlangevin@vericred.com>
2017-05-18 18:51:37 -0400

    Updating nginx version

--- bin/boot.sh
+++ bin/boot.sh
@@ -8,4 +8,4 @@ touch $HOME/logs/nginx/access.log $HOME/logs/nginx/error.log
 
 (tail -f -n 0 $HOME/logs/nginx/*.log &)
 
-exec $HOME/vendor/nginx/sbin/nginx -p $HOME -c $HOME/config/nginx.conf
+exec $HOME/vendor/nginx -p $HOME -c $HOME/config/nginx.conf
